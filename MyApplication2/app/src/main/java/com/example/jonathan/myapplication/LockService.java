8215cd33cd570f68be52037e9688595503f73194
nightjars <nightjars@gmail.com>
2016-11-22 18:37:25 -0800

    added skeleton for lock service

--- MyApplication2/app/src/main/java/com/example/jonathan/myapplication/LockService.java
+++ MyApplication2/app/src/main/java/com/example/jonathan/myapplication/LockService.java
@@ -5,8 +5,8 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.widget.Toast;
 
-public class LockService extends Service {
-
+public class LockService extends Service implements GPSUpdate {
+    GPSData initialGPSLocation = null;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -16,13 +16,28 @@ public class LockService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         // Let it continue running until it is stopped.
-        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        if (Configuration.getLocationHandler() != null){
+            Configuration.getLocationHandler().subscribeUpdates(this);
+            Configuration.getLocationHandler().setAutomaticUpdates(true);
+            this.initialGPSLocation = Configuration.getLocationHandler().retrieveLastGPSData();
+            Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        }
         return START_STICKY;
+    }
+
+    public void receiveUpdate(GPSData data){
+        Toast.makeText(this, "GPS Update Received", Toast.LENGTH_LONG).show();
+    }
+
+    public void gpsDisconnected(){
+        Toast.makeText(this, "GPS Disconnected", Toast.LENGTH_LONG).show();
+        onDestroy();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+        Configuration.getLocationHandler().setAutomaticUpdates(false);
     }
 }
